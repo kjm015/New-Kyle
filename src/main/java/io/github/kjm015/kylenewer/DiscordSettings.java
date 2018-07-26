71fba63f420b8f0e2bc61332ec93a97b0fd1cb76
kjm015 <kjm015@yahoo.com>
2018-07-26 17:56:56 -0500

    added javadoc to DiscordSettings

--- src/main/java/io/github/kjm015/kylenewer/DiscordSettings.java
+++ src/main/java/io/github/kjm015/kylenewer/DiscordSettings.java
@@ -5,6 +5,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+/**
+ * This class sets the properties for this instance of the Discord bot, stored in the application's
+ * property file. I mean, you could store the values in here directly, but that would be stupid.
+ *
+ * Don't do it.
+ *
+ * @author kjm015
+ * @since 7/26/2018
+ */
 @Configuration
 @PropertySource("classpath:application.properties")
 @Getter
