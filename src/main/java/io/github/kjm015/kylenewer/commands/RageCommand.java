caa33fd67c48bd0b2d2f910c6e6fcd10fea858a5
kjm015 <kjm015@yahoo.com>
2018-07-26 18:50:25 -0500

    added javadoc to RageCommand

--- src/main/java/io/github/kjm015/kylenewer/commands/RageCommand.java
+++ src/main/java/io/github/kjm015/kylenewer/commands/RageCommand.java
@@ -2,129 +2,59 @@ package io.github.kjm015.kylenewer.commands;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
+import io.github.kjm015.kylenewer.message.MessageGenerator;
 import lombok.Getter;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
+/**
+ * This command makes Kyle rant and rage about something that he would much rather
+ * not deal with at the current time. It's like, super cereal, guys.
+ *
+ * @author kjm015
+ * @since 7/26/2018
+ */
 @Getter
 public class RageCommand extends Command {
 
     private Random randy = new Random();
 
-    private static final List<String> intents = new ArrayList<String>() {{
-        add("I want to");
-        add("Someone should seriously");
-        add("I should totally");
-        add("Just saying, I'll");
-        add("I am going to");
-        add("Like, I will");
-        add("I'm considering calling someone to");
-        add("I plan to");
-        add("Gonna fucking");
-        add("I should get my dad to");
-        add("Seriously,");
-        add("Why don't I just");
-        add("Even my girlfriend says to");
-        add("I know some guys that would");
+    // Exclamations for Kyle to yell
+    private ArrayList<String> exc = new ArrayList<String>() {{
+        add("shit");
+        add("fuck");
+        add("ass");
+        add("piss");
+        add("dick");
+        add("balls");
+        add("everlasting fucksmack");
     }};
 
-    private static final List<String> open = new ArrayList<String>() {{
-       add("fuck");
-       add("invade");
-       add("roundhouse kick");
-       add("shoot");
-       add("motherfucking sue");
-       add("curb stomp");
-       add("choke slam");
-       add("drop kick");
-       add("kill");
-       add("murder");
-       add("dick punch");
-       add("stab");
-       add("hack");
-       add("exterminate");
-       add("gas");
-       add("protest");
-       add("run over");
-       add("sue");
-       add("vandalize");
-    }};
-
-    private static final List<String> target = new ArrayList<String>() {{
-        add("Ethan Couch");
-        add("Mike Pence");
-        add("Trump");
-        add("Nate");
-        add("Gaijin");
-        add("the Russians");
-        add("these Chinese hackers");
-        add("you fucking idiots");
-        add("Richard Spencer");
-        add("these kids");
-        add("the alt-right");
-        add("my boss");
-        add("the police");
-        add("that guy Seamus");
-        add("the Mamluks");
-        add("the Pope");
-        add("Hitler");
-        add("Nazis");
-        add("stupid people");
-    }};
-
-    private String[] exclamations = {"shit", "everlasting fucksmack", "fuck", "ass", "piss", "dick", "balls"};
-
-    private static final List<String> adjuncts =  new ArrayList<String>() {{
-        add("Like, who fucking though this was a good idea");
-        add("Seriously, where the fuck were you when that happened");
-        add("How is that shit allowed to happen");
-        add("I am not fucking around");
-        add("No one can fucking stop me");
-        add("The system is fucking rigged.");
-        add("We all know it's bullshit.");
-        add("I'm so done with this game's bullshit.");
-        add("Like, I can't fucking stand this shit anymore.");
-    }};
-
-    private static final List<String> closers = new ArrayList<String>() {{
-            add("Not having any of this");
-            add("This is so fucking stupid");
-            add("You guys are fucking stupid");
-            add("I don't give a flying fuck");
-            add("Like what the fuck, dude");
-            add("Literally the dumbest shit");
-            add("I am so fucking done with this");
-            add("This is why we need genocide");
-            add("Like, god fucking damn it");
-            add("We all know it's bullshit.");
-            add("I'm so done with this game's bullshit.");
-            add("Fucking nobody can be okay with this.");
-            add("Like, who fucking though this was a good idea");
-            add("Seriously, where the fuck were you when that happened");
-            add("How is that shit allowed to happen");
-            add("I am not fucking around");
-            add("No one can fucking stop me");
-            add("The system is fucking rigged.");
-            add("We all know it's bullshit.");
-            add("I'm so done with this game's bullshit.");
-    }};
-
+    // Required constructor for all commands
     public RageCommand() {
         this.name = "rage";
         this.aliases = new String[]{"rant.", "rage."};
         this.help = "Kyle will say something in anger";
     }
 
+    /**
+     * The actual things that happen when this command gets run.
+     * In this instance, Kyle replies with a very angry message.
+     *
+     * @param event - the command instance
+     *
+     * @author kjm015
+     * @since 7/26/2018
+     */
     @Override
     protected void execute(CommandEvent event) {
         event.reply(String.format("%s %s the %s out of %s",
-                intents.get(randy.nextInt(intents.size())),
-                open.get(randy.nextInt(open.size())),
-                exclamations[randy.nextInt(exclamations.length)],
-                target.get(randy.nextInt(target.size())))
-        );
-        event.reply(closers.get(randy.nextInt(closers.size())));
+                MessageGenerator.intents.get(randy.nextInt(MessageGenerator.intents.size())),
+                MessageGenerator.open.get(randy.nextInt(MessageGenerator.open.size())),
+                exc.get(randy.nextInt(exc.size()));
+                MessageGenerator.target.get(randy.nextInt(MessageGenerator.target.size()));
+
+        event.reply(MessageGenerator.closers.get(randy.nextInt(MessageGenerator.closers.size())));
     }
 }
--- src/main/java/io/github/kjm015/kylenewer/message/MessageGenerator.java
+++ src/main/java/io/github/kjm015/kylenewer/message/MessageGenerator.java
@@ -1,5 +1,7 @@
 package io.github.kjm015.kylenewer.message;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -12,6 +14,7 @@ import java.util.Random;
  * @author kjm015
  * @since 7/26/2018
  */
+@Getter
 public class MessageGenerator {
 
 	// Random number generator
@@ -401,5 +404,106 @@ public class MessageGenerator {
 
         return temp;
     }
+
+	// List of weird side messages TODO: move to MessageGenerator
+	public static final List<String> adjuncts =  new ArrayList<String>() {{
+		add("Like, who fucking though this was a good idea");
+		add("Seriously, where the fuck were you when that happened");
+		add("How is that shit allowed to happen");
+		add("I am not fucking around");
+		add("No one can fucking stop me");
+		add("The system is fucking rigged.");
+		add("We all know it's bullshit.");
+		add("I'm so done with this game's bullshit.");
+		add("Like, I can't fucking stand this shit anymore.");
+	}};
+
+	// Closing statements. TODO: move to MessageGenerator
+	public static final List<String> closers = new ArrayList<String>() {{
+		add("Not having any of this");
+		add("This is so fucking stupid");
+		add("You guys are fucking stupid");
+		add("I don't give a flying fuck");
+		add("Like what the fuck, dude");
+		add("Literally the dumbest shit");
+		add("I am so fucking done with this");
+		add("This is why we need genocide");
+		add("Like, god fucking damn it");
+		add("We all know it's bullshit.");
+		add("I'm so done with this game's bullshit.");
+		add("Fucking nobody can be okay with this.");
+		add("Like, who fucking though this was a good idea");
+		add("Seriously, where the fuck were you when that happened");
+		add("How is that shit allowed to happen");
+		add("I am not fucking around");
+		add("No one can fucking stop me");
+		add("The system is fucking rigged.");
+		add("We all know it's bullshit.");
+		add("I'm so done with this game's bullshit.");
+	}};
+
+	// List of entry phrases. TODO: Should probably move this to MessageGenerator.
+	public static final List<String> intents = new ArrayList<String>() {{
+		add("I want to");
+		add("Someone should seriously");
+		add("I should totally");
+		add("Just saying, I'll");
+		add("I am going to");
+		add("Like, I will");
+		add("I'm considering calling someone to");
+		add("I plan to");
+		add("Gonna fucking");
+		add("I should get my dad to");
+		add("Seriously,");
+		add("Why don't I just");
+		add("Even my girlfriend says to");
+		add("I know some guys that would");
+	}};
+
+	// List of verbs, TODO: should move to MessageGenerator
+	public static final List<String> open = new ArrayList<String>() {{
+		add("fuck");
+		add("invade");
+		add("roundhouse kick");
+		add("shoot");
+		add("motherfucking sue");
+		add("curb stomp");
+		add("choke slam");
+		add("drop kick");
+		add("kill");
+		add("murder");
+		add("dick punch");
+		add("stab");
+		add("hack");
+		add("exterminate");
+		add("gas");
+		add("protest");
+		add("run over");
+		add("sue");
+		add("vandalize");
+	}};
+
+	// List of intended targets of the action, TODO: should move to MessageGenerator
+	public static final List<String> target = new ArrayList<String>() {{
+		add("Ethan Couch");
+		add("Mike Pence");
+		add("Trump");
+		add("Nate");
+		add("Gaijin");
+		add("the Russians");
+		add("these Chinese hackers");
+		add("you fucking idiots");
+		add("Richard Spencer");
+		add("these kids");
+		add("the alt-right");
+		add("my boss");
+		add("the police");
+		add("that guy Seamus");
+		add("the Mamluks");
+		add("the Pope");
+		add("Hitler");
+		add("Nazis");
+		add("stupid people");
+	}};
 
 }
