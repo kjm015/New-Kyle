03d3c133c97cdd1783a385c59b48ed1587fc3efe
kjm015 <kjm015@yahoo.com>
2018-07-26 18:05:50 -0500

    added javadoc to KyleNewerApplication

--- src/main/java/io/github/kjm015/kylenewer/KyleNewerApplication.java
+++ src/main/java/io/github/kjm015/kylenewer/KyleNewerApplication.java
@@ -14,22 +14,40 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import javax.security.auth.login.LoginException;
 
+/**
+ * Alright, this is the important file.
+ *
+ * This is where the application gets ran, and where all of the commands are imported.
+ * This is where all of the beans are created and where the settings for each instance of the
+ * bot are put into place.
+ *
+ * If your code adds a command, DON'T FORGET TO ADD IT HERE BEFORE YOU PUSH IT!
+ *
+ * @author kjm015
+ * @since 7/26/2018
+ */
 @SpringBootApplication
 @Slf4j
 public class KyleNewerApplication {
 
 	public static void main(String[] args) throws LoginException, IllegalArgumentException {
 
+		// That Spring thing I was talking about earlier ¯\_(ツ)_/¯
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfiguration.class);
 		DiscordSettings settings = applicationContext.getBean(DiscordSettings.class);
 
+		// Import values from settings
 		String token = settings.getOauth();
 		String game = settings.getGame();
 		String owner = settings.getOwner();
 
+		// We could put this all on one line, but we won't. Don't do it.
 		CommandClientBuilder builder = new CommandClientBuilder();
+
+		// Set the bot's prefix (what triggers commands)
 		builder.setPrefix("Hey Kyle, ");
 
+		// Add the commands to the running pool (Add yours here!)
 		builder.addCommand(new RageCommand());
 		builder.addCommand(new AdviceCommand());
 		builder.addCommand(new FetchCommand());
@@ -37,16 +55,20 @@ public class KyleNewerApplication {
 		builder.addCommand(new SuckCommand());
 		builder.addCommand(new RambleCommand());
 
+		// Set the owner of the bot (set in properties)
 		builder.setOwnerId(owner);
 
+		// Build the command listener
 		CommandClient client = builder.build();
 
+		// Build the bot with the given settings
 		new JDABuilder(AccountType.BOT)
 				.setGame(Game.watching(game))
 				.setToken(token)
 				.addEventListener(client)
 				.buildAsync();
 
+		// Run the application that hosts the bot
 		SpringApplication.run(KyleNewerApplication.class, args);
 		log.info("App now running!");
 	}
