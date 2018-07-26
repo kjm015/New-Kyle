3a8dafe20d024bfc46df1cf6c7945f25d6e13e31
kjm015 <kjm015@yahoo.com>
2018-07-26 17:59:04 -0500

    added javadoc to SpringConfig

--- src/main/java/io/github/kjm015/kylenewer/SpringConfiguration.java
+++ src/main/java/io/github/kjm015/kylenewer/SpringConfiguration.java
@@ -5,6 +5,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
+/**
+ * This has something to do with the way that the Discord settings relate to Spring beans.
+ * Not quite sure what this does. I just know that if you remove it, everything breaks.
+ *
+ * Don't do it.
+ *
+ * @since 7/26/2018
+ */
 @Configuration
 @ComponentScan
 public class SpringConfiguration {
