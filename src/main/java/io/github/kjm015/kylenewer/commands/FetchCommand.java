908e0571c5baa7fa731fbb316c4992b0b7c567d3
kjm015 <kjm015@yahoo.com>
2018-07-26 19:07:48 -0500

    added javadoc to FetchCommand

--- src/main/java/io/github/kjm015/kylenewer/commands/FetchCommand.java
+++ src/main/java/io/github/kjm015/kylenewer/commands/FetchCommand.java
@@ -9,11 +9,21 @@ import net.dv8tion.jda.core.entities.User;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * This class makes Kyle fetch some random information about the server or the bot environment.
+ * Feel free to add more argument cases for this method, as right now it doesn't really do all
+ * that much.
+ *
+ * @author kjm015
+ * @since 7/26/2018
+ */
 @Getter
 public class FetchCommand extends Command {
 
-    private Random randy = new Random();
+    // Random number generator
+    private static final Random RANDY = new Random();
 
+    // Required constructor for all Commands
     public FetchCommand() {
         this.name = "fetch";
         this.aliases = new String[] {"retrieve", "get", "return"};
@@ -21,11 +31,24 @@ public class FetchCommand extends Command {
         this.arguments = "<value>";
     }
 
+    /**
+     * This command overrides the abstract method from the Command class.
+     * When this command is called, this is the stuff that actually happens.
+     *
+     * For this class, Kyle, will post a message relating to the specific statistic
+     * that was requested to be gathered for the server.
+     *
+     * @param event - The instance of the command that got called
+     *
+     * @author kjm015
+     * @since 7/26/2018
+     */
     @Override
     protected void execute(CommandEvent event) {
         String args = event.getArgs();
         JDA jda = event.getJDA();
 
+        // Fetch the ping
         if (args.contains("ping") || args.contains("delay") || args.contains("lag")) { // argument for getting delay for New Kyle
             double pingSec = (double)jda.getPing() / 1000.0;
             event.reply("There's like a " + pingSec + " second delay when I post messages");
@@ -37,11 +60,14 @@ public class FetchCommand extends Command {
             } else {
                 event.reply("Holy fucking shit, that's a lot of lag.");
             }
-        } else if (args.contains("members")) {
+        }
+        // Fetch the number of members in the server
+        else if (args.contains("members")) {
             List<User> users = jda.getUsers();
 
             event.reply("I would guess there are like " + users.size() + " guys in this server.");
         }
+        // Don't recognize the command arguments
         else {
             event.replyWarning("No clue what you want from me, dude.");
             event.replyWarning("Like, what is \"" + args +"\" supposed to mean?");
