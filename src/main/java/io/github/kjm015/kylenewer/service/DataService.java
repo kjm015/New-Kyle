11204ba62b1b5f39e6fddacc6dbdcf315d0a4675
Kevin Miyata <kjm015@yahoo.com>
2018-10-11 10:22:33 -0500

    add dummy data service

---
+++ src/main/java/io/github/kjm015/kylenewer/service/DataService.java
@@ -0,0 +1,10 @@
+package io.github.kjm015.kylenewer.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DataService {
+
+}
