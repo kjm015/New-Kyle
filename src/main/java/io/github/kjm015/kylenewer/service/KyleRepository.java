304c6428f59f4cfaa74fc644e9676dec7ba1b21a
kjm015 <kjm015@yahoo.com>
2018-10-28 01:11:06 -0500

    remove h2

--- src/main/java/io/github/kjm015/kylenewer/service/KyleRepository.java
+++
@@ -1,6 +0,0 @@
-package io.github.kjm015.kylenewer.service;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface KyleRepository extends CrudRepository<KyleTable, Long> {
-}
