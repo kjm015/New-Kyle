5fe8d7cacd2b2ebe0ba0df5f9b673f27aac95e0d
kjm015 <kjm015@yahoo.com>
2019-01-03 00:42:19 -0600

    polish stories

--- src/main/java/io/github/kjm015/kylenewer/util/StoryGenerator.kt
+++ src/main/java/io/github/kjm015/kylenewer/util/StoryGenerator.kt
@@ -46,6 +46,8 @@ class StoryGenerator {
         intros.add("The other night, I was in a cab, ")
         intros.add("I was at the bar the other night with a bunch of Chads that were hitting on girls, ")
         intros.add("I was at my girlfriend's place last weekend, ")
+        intros.add("I was sitting on the couch minding my own business, ")
+        intros.add("I was sitting in the airport waiting for my connecting flight, ")
 
         return intros[randy.nextInt(intros.size)]
     }
@@ -58,7 +60,7 @@ class StoryGenerator {
         ants.add("when I corrected my chemistry teacher. ")
         ants.add("when we hear: \"EVERYONE GET DOWN!\" We all get down. ")
         ants.add("and a 17 year old boy was ringing me up. ")
-        ants.add("because the cop touched me first. ")
+        ants.add("when I got bit by a black widow spider twice!")
         ants.add("and Lil Pump was playing on the radio. ")
         ants.add("and all of my employees are having a giant 100-person orgy all over the office. ")
         ants.add("when my sister looked out the window to see me dragging an empty leash around the yard. ")
@@ -66,19 +68,21 @@ class StoryGenerator {
         ants.add("when the nurse asks if I am getting any vaccinations for my son today. ")
         ants.add("and they showed a video of me shoplifting. ")
         ants.add("when an old man tried to shame me because I was on my phone with my headphones in. He said \"Kids today don't read.\" ")
-        ants.add("when all of the kids in the park started searching for the Pokemon I was tracking, even though it disappeared. ")
-        ants.add("and they had the us guess who wrote the poem was on the board. Most people guessed \"Shakespeare\". ")
-        ants.add("when my son's friend said he needs to get shots in order to stay in school.")
+        ants.add("when all of the kids started searching for the Pokemon I was tracking, even though it disappeared. ")
+        ants.add("and they had us guess who wrote the poem was on the board. Most people guessed \"Shakespeare\". ")
+        ants.add("when my son's friend said he needs to get shots in order to stay in school. ")
         ants.add("and my coworkers and I were getting furious at the butcher for slaughtering animals on premises every day. We kept quiet because they kept threatening to fire us. ")
-        ants.add("and a bunch of girls I worked with just flashed me.")
+        ants.add("and a bunch of girls I worked with just flashed me. ")
         ants.add("when I began to write down all of Gandhi's greatest achievements. I wrote that \"He nuked America.\" My teacher asked to see me after class. ")
         ants.add("and the doctor asked why we did not vaccinate our son. ")
-        ants.add("when I began to sing to myself.")
+        ants.add("when I began to sing to myself. ")
         ants.add("and I heard a girl say: \"Boys are so dumb.\" ")
-        ants.add("and we completely ran out of chicken to make McNuggets.")
+        ants.add("and we completely ran out of chicken to make McNuggets. ")
         ants.add("and the cab driver had \"My Life\" by 50 Cent and Eminem on. He asked me if I wanted him to change it to country. ")
-        ants.add("When I see a Chad violently beating a drunk girl. At this point, she's on the floor, cowering and crying.")
-        ants.add("When I played my favorite song for my girlfriend. She said it sounded like \"elevator music\". ")
+        ants.add("when I see a Chad violently beating a drunk girl. At this point, she's on the floor, cowering and crying. ")
+        ants.add("when I played my favorite song for my girlfriend. She said it sounded like \"elevator music\". ")
+        ants.add("when my sister's friend brushed against my thigh to see if I'd notice. ")
+        ants.add("when some blond-haired blue-eyed group of college kids came and asked a Native American where all of his land went. ")
 
         return ants[randy.nextInt(ants.size)]
     }
@@ -99,17 +103,20 @@ class StoryGenerator {
         retorts.add("I tell them that after a lot of research, I have decided not to get any vaccinations. ")
         retorts.add("Just then, an old man around the corner yells out \"Team Rocket blasting off again!\" ")
         retorts.add("My teacher revealed that it was I who wrote it. ")
-        retorts.add("This seems to happen from job to job, too.")
-        retorts.add("He then asked if I played Civilization V, and I said that I did.")
+        retorts.add("This seems to happen from job to job, too. ")
+        retorts.add("He then asked if I played Civilization V, and I said that I did. ")
         retorts.add("My son then told his friend that you do not need shots to be in school, and that they're really dangerous! ")
         retorts.add("I took action, and hid a camera inside their chicken coop. I showed the footage to all the customers while they were eating. ")
         retorts.add("I turned a bit to show him the book in my hand that I was about to read. I said \"I'm just listening to music.\" ")
         retorts.add("My son answers that we don't put poison in our bodies. ")
         retorts.add("I told her to say it louder, and she screamed \"BOYS ARE DUMB!\" ")
-        retorts.add("So, I made 10 batches of vegan chicken nuggets with the ingredients that were on hand.")
-        retorts.add("I responded by rapping the rest of the song word for word. ")
-        retorts.add("I immediately dumped her.")
-        retorts.add("Getting up, I say \"Hey, fuck off man, you have to reason to do that!\" I decked him, and his jaw came off at the hinges, hitting a table on the way down.")
+        retorts.add("So, I made 10 batches of vegan chicken nuggets with the ingredients that were on hand. ")
+        retorts.add("I responded by rapping my favorite song word for word. ")
+        retorts.add("I immediately dumped my girlfriend at that point. ")
+        retorts.add("Getting up, I say \"Hey, fuck off man, you have to reason to do that!\" I decked him, and his jaw came off at the hinges, hitting a table on the way down. ")
+        retorts.add("I didn't have to go to the hospital though, because my fiance is a badass! ")
+        retorts.add("I then life up my dress to reveal the blade I have strapped on my thigh. ")
+        retorts.add("My response to them was \"Hey, how does it feel to be descendants of thieves and murderers and rapists?\" ")
 
         return retorts[randy.nextInt(retorts.size)]
     }
@@ -124,11 +131,10 @@ class StoryGenerator {
         affirm.add("The FEMALE officers were quite pleased with my achievements :) ")
         affirm.add("All other customers started clapping and cheering when \"Rap God\" started playing, and I'd never been so proud. ")
         affirm.add("Everyone went back to work like nothing had happened. A couple commented on how odd the power outage was. ")
-        affirm.add("In my defense, our dog was really fucking small. ")
         affirm.add("It went down so well with a customer that the staff asked for my recipe so they could put it on the menu. ")
         affirm.add("The nurse then tells me that she won't argue with that, and gives me the thumbs up. The doctor prescribes some great vitamin supplements instead. ")
         affirm.add("All the kids groaned and I laughed. He got it right under our noses. ")
-        affirm.add("I then received an applause.")
+        affirm.add("I then received an applause. ")
         affirm.add("Horrified, my son's friend then asked if his parents and doctor knew. My son told him that the doctor gets paid to lie. ")
         affirm.add("People got so mad that they starting throwing their food at the manager. The police showed up and arrested him, and everyone applauded. ")
         affirm.add("Then, he said \"Music today is garbage.\" I showed him my phone. I was listening to hymns. ")
@@ -137,8 +143,8 @@ class StoryGenerator {
         affirm.add("The whole cruise ship clapped for me, and the captain thought it was so good, that he shook my hand. ")
         affirm.add("Other customers replied with a slow clap. ")
         affirm.add("None of the customers realized, and I even heard that they liked it more than usual. ")
-        affirm.add("Hopefully that teaches them not to disrespect women.")
-        affirm.add("It really sucked that my girlfriend had no taste in music.")
+        affirm.add("Hopefully that teaches them not to disrespect women. ")
+        affirm.add("She sucked out all of the spider venom and used herbs as an anti-venom, saving my life! ")
 
         return affirm[randy.nextInt(affirm.size)]
     }
@@ -166,6 +172,8 @@ class StoryGenerator {
         snarks.add("#blessed")
         snarks.add("Best Target experience yet!")
         snarks.add("So yeah, if you could give me that Galaxy Fortnite skin, that would be great.")
+        snarks.add("In my defense, our dog was really fucking small. ")
+        snarks.add("Another reason to grow your own medicine and food!")
 
 
         return snarks[randy.nextInt(snarks.size)]
