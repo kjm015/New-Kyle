29970d6af222b95b12d82371e0fe55f846cf45be
kjm015 <kjm015@yahoo.com>
2019-01-11 14:34:55 -0600

    refactor AppConstants

---
+++ src/main/java/io/github/kjm015/kylenewer/util/AppConstants.kt
@@ -0,0 +1,14 @@
+package io.github.kjm015.kylenewer.util
+
+import java.nio.CharBuffer
+
+object AppConstants {
+
+    const val DISASTER = "\n⣿⣿⣿⣿⡟⠄⣌⠻⣿⣿⣿⣿⣿⠟⠋⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⡇⢸⣭⡇⢽⣿⣿⠏⣀⣶⡇⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣷⣾⢿⣿⣿⣿⣿⣶⣭⣛⢃⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⡛⠈⠛⠁⠙⠉⠛⠿⠛⢟⡿⣿⣷⡝⢿⡿⢻⣿⣿⣿⣿⣿⣿\n⣿⡹⠄⢀⣷⣶⣶⣿⣿⣿⣿⣷⣶⡍⠹⡿⠆⠙⣼⣿⣿⣿⣿⣿⣿\n⢫⣷⣧⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⢀⣴⣶⣏⡛⢿⣿⣿⣿⣿\n⢸⣿⣿⠛⠿⣿⣿⣿⣿⣿⣿⠿⠁⠄⠄⣾⣿⣿⣿⡟⣨⣿⣿⣿⣿\n⡘⣿⣿⣧⣀⣀⣹⣏⢀⣀⣀⣀⣠⡄⢸⣿⣿⣿⣿⢀⣿⣿⣿⣿⣿\n⣷⣼⣋⠈⣿⣿⣿⣿⣿⣿⣿⣿⠟⠄⠈⠛⢿⠏⢙⠈⠁⠄⠙⣿⣿\n⣿⣿⣿⠄⠹⠟⠛⠉⠡⠿⣿⡏⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿\n⣿⣿⠿⠃⠄⠄⣀⡀⠄⠄⠈⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢹\n⠄⠄⢀⡆⣰⠟⠷⣤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n⠄⢀⠎⠄⠃⢀⠞⠉⢳⣴⣶⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣀"
+
+    val KYLE_LOCATION_QUESTIONS = arrayOf("Where is Kyle", "What happened to Kyle", "Where did Kyle go", "Where's Kyle", "凯尔在哪")
+
+    val FLAG_TERM_1: CharSequence = CharBuffer.wrap(charArrayOf(78.toChar(), 73.toChar(), 71.toChar(), 71.toChar(), 69.toChar(), 82.toChar()))
+    val FLAG_TERM_2: CharSequence = CharBuffer.wrap(charArrayOf(68.toChar(), 69.toChar(), 86.toChar(), 69.toChar(), 78.toChar()))
+
+}
