f8c954f77f02aa095dfd43938c2019427527e621
kjm015 <kjm015@yahoo.com>
2019-01-20 20:19:30 -0600

    clean lyrics command response

--- src/main/kotlin/io/github/kjm015/kylenewer/commands/LyricsCommand.kt
+++ src/main/kotlin/io/github/kjm015/kylenewer/commands/LyricsCommand.kt
@@ -6,7 +6,17 @@ import com.jagrosh.jlyrics.LyricsClient
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-class LyricsCommand: Command() {
+/**
+ * This command allows Kyle to post lyrics of a song. He will display the name of the song, the author,
+ * and the contents of the lyrics.
+ *
+ * Lyrics API is courtesy of John A. Grosh.
+ * See <a href="https://github.com/jagrosh">https://github.com/jagrosh</a>
+ *
+ * @author kjm015
+ * @since 01/20/2019
+ */
+class LyricsCommand : Command() {
 
     private val log: Logger = LoggerFactory.getLogger(this.javaClass)
 
@@ -14,17 +24,23 @@ class LyricsCommand: Command() {
 
     init {
         this.name = "lyrics"
-        this.arguments = "<song>"
+        this.arguments = "<song name>"
         this.help = "Kyle will get some lyrics for a song of your choice"
         this.aliases = arrayOf("sing", "song")
     }
 
     override fun execute(event: CommandEvent) {
+        // Kyle will say that he is typing...
+        event.channel.sendTyping()
+
+        // Attempt to retrieve lyrics
         try {
-            event.reply(lyrics.getLyrics(event.args).get().content)
+            val song = lyrics.getLyrics(event.args).get()
+            event.reply("Getting ${song.title} by ${song.author}...\n\n")
+            event.reply(song.content)
         } catch (e: Exception) {
             log.error("Could not fetch lyrics: $e")
-            event.reply("Couldn't find anything named \"${event.args}\".")
+            event.replyError("I don't know any song named \"${event.args}\".")
         }
     }
 
