2c3dba67fa7c85ef69ed7180d32b565f7303feec
Chiengbang <ichiengbang@gmail.com>
2020-07-05 04:49:50 -0500

    Changes done: changed help message, changed event.reply() -> event.replyError(), documentation of EchoCommand class

--- src/main/kotlin/io/github/kjm015/kylenewer/commands/EchoCommand.kt
+++ src/main/kotlin/io/github/kjm015/kylenewer/commands/EchoCommand.kt
@@ -4,17 +4,24 @@ import com.jagrosh.jdautilities.command.Command
 import com.jagrosh.jdautilities.command.CommandEvent
 import org.springframework.stereotype.Component
 
+/**
+ * This command will let Kyle mock you by saying exactly what you just said
+ * 
+ * @author ChiengBang
+ * @since 07/05/2020
+*/
+
 @Component
 class EchoCommand : Command() {
     init {
         this.name = "echo"
-        this.help = "Tells Kyle whatever you want"
-        this.arguments = "a message"
+        this.help = "Kyle will mock you by saying exactly what you just said"
+        this.arguments = "text"
     }
 
     override fun execute(event: CommandEvent) {
         if (event.args.isBlank()) {
-            event.reply("Type something dummy")
+            event.replyError("Type something dummy")
         } else {
             event.reply(event.args)
         }
