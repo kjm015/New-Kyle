17441bb9b28fd1b5abdf1c176d4933c6a655ed24
kjm015 <kjm015@yahoo.com>
2021-08-23 19:14:50 -0500

    move cpu loader to json

--- src/main/kotlin/io/github/kjm015/kylenewer/util/computer/cpu/CPU.kt
+++ src/main/kotlin/io/github/kjm015/kylenewer/util/computer/cpu/CPU.kt
@@ -1,25 +1,27 @@
 package io.github.kjm015.kylenewer.util.computer.cpu
 
-import io.github.kjm015.kylenewer.util.computer.cooler.CPUCooler
 import javax.persistence.*
-import kotlin.random.Random
 
 @Entity
 data class CPU(
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(unique = true)
-        var id: Long = -1L,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    var id: Long = -1L,
 
-        val company: String = "Unknown",
-        val name: String = "CPU",
-        val coreCount: Int = -1,
-        val threadCount: Int = -1,
-        val lithography: Int = -1,
-        val socket: String = "Unknown",
-        val integratedGraphics: String? = null,
-        val includesCooler: Boolean = false,
-        val includedCoolerName: String? = null,
+    val company: String = "Unknown",
+    val name: String = "CPU",
+    val coreCount: Int = -1,
+    val threadCount: Int = -1,
+    val lithography: Int = -1,
+    val socket: String = "Unknown",
+    val integratedGraphics: String? = null,
+    val includesCooler: Boolean = false,
+    val includedCoolerName: String? = null,
 
-        var price: Double = 0.0
+    var price: Double = 0.0
 )
+
+class CPUList {
+    var processors: List<CPU> = listOf()
+}
--- src/main/kotlin/io/github/kjm015/kylenewer/util/computer/cpu/CPULoader.kt
+++ src/main/kotlin/io/github/kjm015/kylenewer/util/computer/cpu/CPULoader.kt
@@ -1,149 +1,26 @@
 package io.github.kjm015.kylenewer.util.computer.cpu
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
+import org.springframework.util.ResourceUtils
 
 @Component
 class CPULoader(private val cpuRepository: CPURepository) : ApplicationRunner {
+
+    private val mapper = ObjectMapper()
+
     override fun run(args: ApplicationArguments) {
-        listOf(
-                CPU(
-                        company = "AMD",
-                        name = "Ryzen 3 3100",
-                        coreCount = 4,
-                        threadCount = 8,
-                        lithography = 7,
-                        socket = "AM4",
-                        includesCooler = true,
-                        includedCoolerName = "Wraith Stealth",
-                        price = 108.99
-                ),
-                CPU(
-                        company = "AMD",
-                        name = "Ryzen 3 3300X",
-                        coreCount = 4,
-                        threadCount = 8,
-                        lithography = 7,
-                        socket = "AM4",
-                        includesCooler = true,
-                        includedCoolerName = "Wraith Stealth",
-                        price = 129.99
-                ),
-                CPU(
-                        company = "AMD",
-                        name = "Ryzen 5 3600",
-                        coreCount = 6,
-                        threadCount = 12,
-                        lithography = 7,
-                        socket = "AM4",
-                        includesCooler = true,
-                        includedCoolerName = "Wraith Stealth",
-                        price = 174.99
-                ),
-                CPU(
-                        company = "AMD",
-                        name = "Ryzen 5 3600X",
-                        coreCount = 6,
-                        threadCount = 12,
-                        lithography = 7,
-                        socket = "AM4",
-                        includesCooler = true,
-                        includedCoolerName = "Wraith Spire",
-                        price = 209.99
-                ),
-                CPU(
-                        company = "AMD",
-                        name = "Ryzen 5 3600XT",
-                        coreCount = 6,
-                        threadCount = 12,
-                        lithography = 7,
-                        socket = "AM4",
-                        includesCooler = true,
-                        includedCoolerName = "Wraith Spire",
-                        price = 239.99
-                ),
-                CPU(
-                        company = "AMD",
-                        name = "Ryzen 7 3700X",
-                        coreCount = 8,
-                        threadCount = 16,
-                        lithography = 7,
-                        socket = "AM4",
-                        includesCooler = true,
-                        includedCoolerName = "Wraith Prism",
-                        price = 283.00
-                ),
-                CPU(
-                        company = "AMD",
-                        name = "Ryzen 7 3800X",
-                        coreCount = 8,
-                        threadCount = 16,
-                        lithography = 7,
-                        socket = "AM4",
-                        includesCooler = true,
-                        includedCoolerName = "Wraith Prism",
-                        price = 329.99
-                ),
-                CPU(
-                        company = "AMD",
-                        name = "Ryzen 7 3800XT",
-                        coreCount = 8,
-                        threadCount = 16,
-                        lithography = 7,
-                        socket = "AM4",
-                        price = 398.99
-                ),
-                CPU(
-                        company = "AMD",
-                        name = "Ryzen 9 3900X",
-                        coreCount = 12,
-                        threadCount = 24,
-                        lithography = 7,
-                        socket = "AM4",
-                        includesCooler = true,
-                        includedCoolerName = "Wraith Prism",
-                        price = 428.90
-                ),
-                CPU(
-                        company = "AMD",
-                        name = "Ryzen 9 3950X",
-                        coreCount = 16,
-                        threadCount = 32,
-                        lithography = 7,
-                        socket = "AM4",
-                        price = 689.99
-                ),
-                CPU(
-                        company = "AMD",
-                        name = "Threadripper 3960X",
-                        coreCount = 24,
-                        threadCount = 48,
-                        lithography = 7,
-                        socket = "sTRX4",
-                        price = 1349.99
-                ),
-                CPU(
-                        company = "AMD",
-                        name = "Threadripper 3970X",
-                        coreCount = 32,
-                        threadCount = 64,
-                        lithography = 7,
-                        socket = "sTRX4",
-                        price = 1853.37
-                ),
-                CPU(
-                        company = "AMD",
-                        name = "Threadripper 3990X",
-                        coreCount = 64,
-                        threadCount = 128,
-                        lithography = 7,
-                        socket = "sTRX4",
-                        price = 3564.99
-                )
-        ).forEach {
+        retrieveFromFile().forEach {
             if (!cpuRepository.existsByName(it.name))
                 cpuRepository.save(it)
         }
+    }
+
+    private fun retrieveFromFile(): List<CPU> {
+        val file = ResourceUtils.getFile("classpath:computer/cpu.json")
+        val list = mapper.readValue(file, CPUList::class.java)
+        return list.processors
     }
 }
