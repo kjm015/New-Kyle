be74797c7a6f3b483604cc784ef2c72f4b860cad
kjm015 <kjm015@yahoo.com>
2021-09-10 02:17:00 -0500

    move storage to file

--- src/main/kotlin/io/github/kjm015/kylenewer/util/computer/storage/Storage.kt
+++ src/main/kotlin/io/github/kjm015/kylenewer/util/computer/storage/Storage.kt
@@ -1,10 +1,14 @@
 package io.github.kjm015.kylenewer.util.computer.storage
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import javax.persistence.*
 
 @Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Storage (
         @Id
+        @JsonIgnore
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Column(unique = true)
         val id: Long = -1L,
@@ -16,8 +20,15 @@ data class Storage (
         val formFactor: String = "Unknown",
         val boardInterface: String = "Unknown",
         val isNVMe: Boolean = false,
+        val pcieGeneration: Int = -1,
+        val maxReadSpeedMBps: Long = 0,
+        val maxWriteSpeedMBps: Long = 0,
 
         var price: Double = 0.0
 ) {
         fun productName(): String = "$manufacturer $name"
+}
+
+class StorageList {
+        var drives: List<Storage> = listOf()
 }
