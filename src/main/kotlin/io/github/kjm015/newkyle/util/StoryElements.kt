82d48ffb84ed17592941f7409fc6d931a3908bee
kjm015 <kjm015@yahoo.com>
2022-05-11 21:45:58 -0500

    simplify story command

---
+++ src/main/kotlin/io/github/kjm015/newkyle/util/StoryElements.kt
@@ -0,0 +1,9 @@
+package io.github.kjm015.newkyle.util
+
+data class StoryElements(
+    val setups: List<String> = listOf(),
+    val foils: List<String> = listOf(),
+    val replies: List<String> = listOf(),
+    val affirmations: List<String> = listOf(),
+    val flourishes: List<String> = listOf()
+)
