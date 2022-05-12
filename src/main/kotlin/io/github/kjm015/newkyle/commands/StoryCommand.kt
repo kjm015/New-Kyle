2e9fe8dd56d5d796283142cbf8e5bde6f2903ed4
kjm015 <kjm015@yahoo.com>
2022-05-12 12:15:51 -0500

    add typing queue to story command

--- src/main/kotlin/io/github/kjm015/newkyle/commands/StoryCommand.kt
+++ src/main/kotlin/io/github/kjm015/newkyle/commands/StoryCommand.kt
@@ -4,13 +4,10 @@ import com.jagrosh.jdautilities.command.Command
 import com.jagrosh.jdautilities.command.CommandEvent
 import io.github.kjm015.newkyle.repository.StoryEntry
 import io.github.kjm015.newkyle.util.StoryService
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 /**
- * Kyle will generate a story from the StoryService class. These stories are
- * compiled from the top posts on reddit.com/r/thatHappened, which is basically
- * a series of hilarious "true" stories.
+ * Kyle will generate a story from the StoryService class.
  *
  * @author kjm015
  * @since 01/20/2019
@@ -18,26 +15,28 @@ import org.springframework.stereotype.Component
 @Component
 class StoryCommand(private val generator: StoryService) : Command() {
 
-    init {
-        this.name = "story"
-        this.aliases = arrayOf("tale", "fable")
-        this.help = "Kyle will tell you a story about a recent occurrence in his life."
-        this.guildOnly = false
-    }
+  init {
+    this.name = "story"
+    this.aliases = arrayOf("tale", "fable")
+    this.help = "Kyle will tell you a story about a recent occurrence in his life."
+    this.guildOnly = false
+  }
 
-    override fun execute(event: CommandEvent) = when {
-
-        event.args.startsWith(prefix = "get", ignoreCase = true) -> {
-            event.reply("Let me check what I've got...\n")
-            generator.getAllStoryComponents().sortedBy {
-                it.category
-            }.forEachIndexed { i: Int, storyEntry: StoryEntry ->
-                event.reply("${i + 1}. ${storyEntry.category.replaceFirstChar { it.uppercase() }} by ${storyEntry.author}: \"${storyEntry.text}\"")
-            }
-
-            event.replySuccess("\nThat's all I've got.")
+  override fun execute(event: CommandEvent) {
+    event.channel.sendTyping().queue()
+    when {
+      event.args.startsWith(prefix = "get", ignoreCase = true) -> {
+        event.reply("Let me check what I've got...\n")
+        generator.getAllStoryComponents().sortedBy {
+          it.category
+        }.forEachIndexed { i: Int, storyEntry: StoryEntry ->
+          event.reply("${i + 1}. ${storyEntry.category.replaceFirstChar { it.uppercase() }} by ${storyEntry.author}: \"${storyEntry.text}\"")
         }
 
-        else -> event.reply(generator.story())
+        event.replySuccess("\nThat's all I've got.")
+      }
+
+      else -> event.reply(generator.story())
     }
+  }
 }
